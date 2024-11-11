53a87b3f4e2f924f90a876bd8b14a808c6a5b2e2
Andy Stevens <andrew.stevens@gtri.gatech.edu>
2024-11-11 14:14:28 -0500

    First push of new template

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
-import { defineConfig } from 'astro/config'
-import compress from 'astro-compress'
-import mdx from '@astrojs/mdx'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
+import sitemap from '@astrojs/sitemap';
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [compress(), mdx(), tailwind()],
-})
+  site: 'https://astrofy-template.netlify.app',
+  integrations: [mdx(), sitemap(), tailwind()]
+});
---
+++ src/config.ts
@@ -0,0 +1,7 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'andystevens.dev';
+export const SITE_DESCRIPTION = 'Andy Stevens Personal Portfolio Website';
+export const GENERATE_SLUG_FROM_TITLE = true
+export const TRANSITION_API = true
